21e782d9801951373665a423821535003ad11147
yasg-uru <yashpawar12122004@gmail.com>
2024-06-17 17:02:42 +0530

    removed unused axios

--- src/Redux_toolkit/ExpenseSlice.tsx
+++ src/Redux_toolkit/ExpenseSlice.tsx
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+
 import ExpenseInterface from "../interfaces/ExpenseSliceInterface";
 import toast from "react-hot-toast";
 import CreateExpenseInterface from "../interfaces/CreateExpenseInterface";
