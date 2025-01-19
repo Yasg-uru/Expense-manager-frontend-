78c92a5acc6c608aab867c7e69d972b0bf720036
yasg-uru <yashpawar12122004@gmail.com>
2025-01-19 21:51:46 +0530

    changed basurl

--- src/helpers/axiosinstance.tsx
+++ src/helpers/axiosinstance.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-const baseURL: string = "http://localhost:8000";
-// const baseURL: string = "https://expense-manager-backend-orcin.vercel.app";
+// const baseURL: string = "http://localhost:8000";
+const baseURL: string = "https://expense-manager-backend-orcin.vercel.app";
 const axiosInstance = axios.create({
   baseURL,
 });
